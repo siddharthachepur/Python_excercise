46fe4d95d122a82d2eea58673c569192dfd3d74a
siddhartha.chepur <siddhartha.chepur@fireeye.com>
2017-02-05 18:43:21 -0800

    Code for Number Complement

--- NumberComplement.py
+++ NumberComplement.py
@@ -4,4 +4,5 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
+        # The complement strategy is to flip the bits of its binary representation.
         return int(bin(num)[2:].replace('0', '@').replace('1', '0').replace('@', '1'), 2)
