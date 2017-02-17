8352ff612effcca4d17ad469c1c54461b2f7d1d0
siddhartha.chepur <siddhartha.chepur@fireeye.com>
2017-02-16 18:01:39 -0800

    Code for Sum of 2 integers without using + operator

---
+++ Sum_of_Two_Integers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        How to add 2 integers without using '+' or '-' operators
+        """
+        x = []
+        x.append(a)
+        x.append(b)
+        return sum(x)
